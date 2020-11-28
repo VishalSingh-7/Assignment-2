55ee7dca5956a176b281565b00a087884af758f9
VishalSingh-7 <74832202+VishalSingh-7@users.noreply.github.com>
2020-11-28 20:22:59 +0530

    Write a C program to print all alphabets from a to z
    This C program to print all alphabets from a to z.

---
+++ Problem1.c
@@ -0,0 +1,11 @@
+//  Write a C program to print all alphabets from a to z.
+#include <stdio.h>
+int main()
+{
+    char c;
+    for(c='a';c<='z';c++)
+    {
+        printf(" %c ",c);
+    }
+    return 0;
+}
